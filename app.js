839d601886e19bf1c297097fb7fc109b96514833
hafizsajidhameed <hafizsajidhameed@gmail.com>
2024-03-20 22:41:35 +0100

    6th commit

--- app.js
+++ app.js
@@ -26,6 +26,28 @@ const products = [
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// a middleware example it would be used in all routes to log reqest method
+const middleware1 = (req, res, next)=> {
+  console.log(`this is ${req.method} request, and URL ${req.url}`);
+  next()
+}
+
+//middleware for find user index
+const getUserIndexByUsername = (req, res, next)=> {
+  const {params : {id}} = req;
+  const parsedId = parseInt(id);
+  if(isNaN(parseInt(id))) return res.status(400).send('incalid id');
+  const findUserIndex = users.indexOf(users.find(user => user.id === parsedId));
+  next()
+}
+//middleware used globly
+app.use(middleware1)
+
+// middleware example2
+const middleware2 = (req, res, next)=> {
+  console.log('object addedk into array successfully')
+  next()
+}
 app.get("/", (req, res) => {
     console.log(req.query);
   res.status(201).send({message : 'Hello World'})
@@ -47,7 +69,7 @@ app.get('/api/users', (req, res) => {
 });
 
 // users post request
-app.post('/api/users', (req, res)=> {
+app.post('/api/users',middleware2, (req, res)=> {
   console.log(req.body)
   const {body} = req;
   const newUser = {id: users[users.length - 1].id + 1, ...body};
@@ -55,6 +77,7 @@ app.post('/api/users', (req, res)=> {
   return res.status(201).send(users);
 })
 
+//find the user by it's id
 app.get('/api/users/:id', (req, res) => {
   const parsedId = parseInt(req.params.id);
   if (isNaN(parsedId)) return res.status(400).send({message: 'bad request, invalid id.'})
@@ -64,17 +87,37 @@ app.get('/api/users/:id', (req, res) => {
 });
 
 // put request for updating the object
-// app.put('/api/users/:id', (req, res)=> {
-//   const {body, params: {id},} = req;
-//   const parsedId  =  parseInt(id);
-//   if(isNaN(parsedId)) return res.status(400).send('Bad Request');
+app.put('/api/users/:id', (req, res)=> {
+  const {body, params: {id},} = req;
+  
+  users[findUserIndex] = {id: parsedId, ...body };
+  return res.status(200).send(users[findUserIndex]);
+});
 
-//   const findUserIndex = users.findIndex((user)=> user.id === parsedId);
+//patch request for updating partially
+app.patch('/api/users/:id', getUserIndexByUsername, (req,res)=>{
+  const { body, findUserIndex} = req;
+//   const parsedId = parseInt(id);
+//  if(isNaN(parsedId))return res.status(400).send('Invalid Id');
+//  const findUserIndex = users.findIndex(user=> user.id===parsedId);
+//  if(findUserIndex === -1) return res.status(404);
+ users[findUserIndex] = {...users[findUserIndex],...body};
+ res.status(200).json(users[findUserIndex])
+});
 
-//   if(findUserIndex === -1 ) return res.status(404).send("Not Found");
-//   users [findUserIndex] = {id: parsedId, ...body };
-//   return res.status(200).send(users[findUserIndex]);
-// })
+
+//delete a user from array of objects
+app.delete('/api/users/:id', getUserIndexByUsername, (req , res)=> {
+  // const {params : {id}} = req;
+  // const parsedId = parseInt(id);
+  // if(isNaN(parseInt(id))) return res.status(400).send('incalid id');
+  // const findUserIndex = users.indexOf(users.find(user => user.id === parsedId));
+  // if(findUserIndex===-1) return res.status(404).send('not found');
+  
+    users.splice(findUserIndex,1);
+    return res.status(200).send(`The user with id ${parsedId} has been deleted`);
+  }
+)
 
 
 
